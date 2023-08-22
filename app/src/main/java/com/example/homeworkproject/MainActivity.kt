878b16dfe642bc7aa8be7fd938d7f2ba06ebd42c
Leonardo_13 <leonardo.novak5@gmail.com>
2023-08-22 09:16:41 +0200

    Main fragment added using fragmentManager

--- app/src/main/java/com/example/homeworkproject/MainActivity.kt
+++ app/src/main/java/com/example/homeworkproject/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.homeworkproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentManager = supportFragmentManager
+
+    override fun onStart() {
+        super.onStart()
+
+        fragmentManager.commit {
+            add(R.id.fragment_container, MainFragment())
+            addToBackStack(null)
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
 }
